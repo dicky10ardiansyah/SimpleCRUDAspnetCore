f985a8e1a3a0313f697dad2ddeca53b3678caf72
dicky_ardiansyah <eigerdlamborghini.ardiansyah@gmail.com>
2023-06-05 21:48:59 +0700

    CRUD Asp.net Core v1

---
+++ AspNetCRUD/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AspNetCRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCRUD.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options) 
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ AspNetCRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
