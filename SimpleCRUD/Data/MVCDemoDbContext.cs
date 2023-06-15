b30824c061a2483d6108f35f80b369002c990e61
dicky_ardiansyah <eigerdlamborghini.ardiansyah@gmail.com>
2023-06-15 23:42:54 +0700

    CRUD Asp.net Core v1.1

---
+++ SimpleCRUD/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleCRUD.Models.Domain;
+
+namespace SimpleCRUD.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
